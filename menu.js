ef3d1f2047ad25c0f0c24835689c7f84ce69b7fd
HAR0044 <HAR0044@jmss.vic.edu.au>
2023-02-22 11:44:05 +1100

    website sample stuff
    stuff i found on my laptop for us to use :)

---
+++ menu.js
@@ -0,0 +1,13 @@
+showMenu = function() {
+  var subMenuItems = document.getElementById("topMenu").getElementsByTagName("li");
+  for (var i=0; i<subMenuItems.length; i++) {
+    subMenuItems[i].onmouseover=function() {
+      this.className+=" showMenu";
+    }
+    subMenuItems[i].onmouseout=function() {
+      this.className=this.className.replace(new RegExp(" showMenu\\b"), "");
+    }
+  }
+}
+	
+if (window.attachEvent) window.attachEvent("onload", showMenu);
